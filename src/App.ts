2d99009f0f4f1f568364bc15e8cc023469ed459a
hllowrld <ira.bcit@protonmail.com>
2021-04-09 14:49:44 -0700

    checking other branch, saving changes

--- src/App.ts
+++ src/App.ts
@@ -44,7 +44,7 @@ class App {
         cookie: {
           httpOnly: true,
           secure: false,
-          maxAge: 24 * 60 * 1000,
+          maxAge: 24 * 60 * 60 * 1000,
         }
       })
     )
